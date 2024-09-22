696c6a6d724899136c0c71c68c47412ed0f65988
akamirthaa <ak.amirthaa@gmail.com>
2024-09-22 19:54:11 +0530

    Update check_limits.py - testing last assert

--- check_limits.py
+++ check_limits.py
@@ -24,4 +24,4 @@ def battery_is_ok(temperature, soc, charge_rate):
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 1) is False)
-  assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(25, 80, 0) is False)
