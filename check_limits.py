199ee9c5d707717355ce5a6d63aeef81051423ba
akamirthaa <ak.amirthaa@gmail.com>
2024-10-14 02:51:23 +0530

    add main

--- check_limits.py
+++ check_limits.py
@@ -4,3 +4,6 @@ from state_of_charge import check_soc_limit
 
 def is_battery_ok(temperature, soc, charge_rate):
     return (check_temperature_limit(temperature) and check_soc_limit(soc) and check_charge_rate_limit(charge_rate))
+
+if __name__ == '__main__':
+    is_battery_ok(20, 40, 0.7)
